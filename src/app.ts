7f4c527e989f28d492bc073f7bf84545b24ec9cf
Perthanakrit <aupper094@gmail.com>
2025-11-04 08:54:25 +0700

    add users route

--- src/app.ts
+++ src/app.ts
@@ -5,27 +5,35 @@ const app: Express = express()
 const port: number = 3000
 
 // Routes
- // GET /
- app.get('/', (_: Request, res: Response) => {
-   res.json({
-       message: 'Hello Express + TypeScript!'
-         })
-         })
+// GET /
+app.get('/', (_: Request, res: Response) => {
+  res.json({
+    message: 'Hello Express + TypeScript!'
+  })
+})
 
-         // GET /api/hello
-         app.get('/api/hello', (_: Request, res: Response) => {
-           res.json({
-               message: 'Hello from Express API!'
-                 })
-                 })
+// GET /api/hello
+app.get('/api/hello', (_: Request, res: Response) => {
+  res.json({
+    message: 'Hello from Express API!'
+  })
+})
 
-                 // GET /api/health
-                 app.get('/api/health', (_: Request, res: Response) => {
-                   res.json({
-                       status: 'UP'
-                        })
-                         })
+// GET /api/health
+app.get('/api/health', (_: Request, res: Response) => {
+  res.json({
+    status: 'UP'
+  })
+})
 
-                         // Start server
-                         app.listen(port, () => console.log(`Application is running on port ${port}`))
+app.get('/api/users', (_: Request, res: Response) => {
+  const users = [
+    { id: 1, name: 'Jone Doe'},
+    { id: 2, name: 'Jane Doe'}
+  ]
+  res.json(users)
+})
+
+// Start server
+app.listen(port, () => console.log(`Application is running on port ${port}`))
 
